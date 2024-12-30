ec500c12867e600f48c7775e56c63b25ecf58e59
John Doe <johndoe@example.com>
2024-12-30 10:13:02 +0100

    done

--- Film.php
+++ Film.php
@@ -16,10 +16,49 @@ class Film {
         $this->realisateur = $realisateur;
     }
 
-    public function ajouterFilm() {}
+    public function ajouterFilm(&$films) {
+        $films[] = [
+            'id' => $this->id,
+            'titre' => $this->titre,
+            'genre' => $this->genre,
+            'duree' => $this->duree,
+            'dateSortie' => $this->dateSortie,
+            'realisateur' => $this->realisateur,
+        ];
+    }
 
-    public function modifierFilm() {}
+    public function modifierFilm(&$films) {
+        foreach ($films as &$film) {
+            if ($film['id'] === $this->id) {
+                $film['titre'] = $this->titre;
+                $film['genre'] = $this->genre;
+                $film['duree'] = $this->duree;
+                $film['dateSortie'] = $this->dateSortie;
+                $film['realisateur'] = $this->realisateur;
+                return;
+            }
+        }
+        echo "Film introuvable.\n";
+    }
 
-    public function supprimerFilm() {}
+    public function supprimerFilm(&$films) {
+        foreach ($films as $index => $film) {
+            if ($film['id'] === $this->id) {
+                unset($films[$index]);
+                return;
+            }
+        }
+        echo "Film introuvable.\n";
+    }
 }
+
+$films = [];
+
+$film1 = new Film(1, "Avatar", "Science-fiction", 162, "2009-12-18", "James Cameron");
+$film1->ajouterFilm($films);
+
+$film2 = new Film(1, "Avatar: The Way of Water", "Science-fiction", 192, "2022-12-16", "James Cameron");
+$film2->modifierFilm($films);
+
+print_r($films);
 ?>
